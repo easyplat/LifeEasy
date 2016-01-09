047d53d2506e8e4b924e1e5c64f5e5f677b10898
Fish <429081849@qq.com>
2016-01-09 15:26:42 +0800

    修改spring mvc配置文件

---
+++ langeye/src/main/java/com/langeye/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.langeye.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.langeye.vo.User;
+
+@Controller
+@RequestMapping(value="/admin")
+public class AdminController {
+
+		
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public String index(){
+		
+		return "index";
+	}
+	
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String loginGet(){
+		
+		return "login";
+	}
+	
+	@RequestMapping(value="/login",method=RequestMethod.POST)
+	public String loginPost(@ModelAttribute("user") User user){
+		return "redirect:/admin/index";		
+	}	
+}
---
+++ langeye/src/main/java/com/langeye/vo/User.java
@@ -0,0 +1,5 @@
+package com.langeye.vo;
+
+public class User {
+
+}
